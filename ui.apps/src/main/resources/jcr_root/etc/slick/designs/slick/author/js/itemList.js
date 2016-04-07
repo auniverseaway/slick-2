29bbd3f4880e79187263dc3415f67bbd6f53a6f8
Chris Millar <cmillar@adobe.com>
2016-04-07 15:44:48 -0600

    Support for deleting posts. Login json update.

---
+++ ui.apps/src/main/resources/jcr_root/etc/slick/designs/slick/author/js/itemList.js
@@ -0,0 +1,41 @@
+// Delete
+[].forEach.call(document.querySelectorAll('.icon.delete'), function (element) {
+    element.addEventListener('click', function(event) {
+        event.preventDefault();
+        var confirmDelete = element.nextElementSibling;
+        confirmDelete.classList.toggle('open');
+        element.classList.toggle('cancel');
+    });
+});
+
+// Confirm Delete
+[].forEach.call(document.querySelectorAll('.delete-confirm'), function (element) {
+    element.addEventListener('click', function(event) {
+        event.preventDefault();
+        sendDeleteRequest(element.dataset.resourcePath, element.dataset.resourceName);
+    });
+});
+
+// Send the Delete Request
+function sendDeleteRequest(resourcePath, resourceName) {
+	var formAction = resourcePath;
+    var formData = new FormData();
+    formData.append(":operation", "delete");
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+    	var msg;
+    	if (xhr.readyState == 4) {
+    		if(xhr.status == 200) {
+            	msg = { 'responseCode': xhr.status, 'responseType':'success', 'responseMessage': 'Post successfully deleted' };
+            	var deletedResource = document.querySelector("#" + resourceName);
+            	deletedResource.parentNode.removeChild(deletedResource);
+            } else {
+            	msg = { 'responseCode': xhr.status, 'responseType':'error', 'responseMessage': 'Something went wrong' };
+            }
+    		sendMessage(msg);
+    	}    		
+    	
+    };
+    xhr.open("POST", formAction, true);
+    xhr.send(formData);
+}
