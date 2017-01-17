0c1b7cc98feb8fb16a9dfeccf5fe101e08384872
Chris Millar <cmillar@adobe.com>
2017-01-17 13:41:05 -0700

    Add a few params to Adobe Analytics events.

--- ui.apps/src/main/resources/jcr_root/etc/slick/designs/slick/src/js/publish/social.js
+++ ui.apps/src/main/resources/jcr_root/etc/slick/designs/slick/src/js/publish/social.js
@@ -1,41 +1,55 @@
-/*global window, define, FileReader */
+/*global window, define */
 /**
  * Analytics, Social, and SEO Functions.
  *
+ * <p>This module will handle new windows for social sharing.</p>
+ * <p>If analytics are enabled, send the appropriate events to those services</p>
  */
 define(function () {
     'use strict';
 
-    var analyticsType;
+    var analyticsType = document.body.dataset.analyticsType;
 
-    var trackLink = function (link, socialNetwork) {
+    /**
+     * Send social information to analytics services.
+     * @param  {String} shareUri      The URL we're sharing.
+     * @param  {String} socialNetwork The social network.
+     * @return {void}
+     */
+    var trackLink = function (shareUri, socialNetwork) {
         if (analyticsType === 'adobeAnalytics') {
-            var appMeasurement = window.s_c_il[0];
-            if (appMeasurement) {
-                appMeasurement.tl(link, 'o', socialNetwork);
+            var s = window.s_c_il[0];
+            if (s) {
+                s.tl(shareUri, 'o', socialNetwork, {linkTrackVars: 'prop1', prop1: s.pageName});
             }
         } else if (analyticsType === 'googleAnalytics') {
             if (window.ga) {
-                window.ga('send', {
-                    hitType: 'social',
-                    socialNetwork: socialNetwork,
-                    socialAction: 'Share',
-                    socialTarget: link
-                });
+                window.ga('send', 'social', socialNetwork, 'Share', shareUri);
             }
         }
     };
 
-    // Share Windows
-    [].forEach.call(document.querySelectorAll('.social'), function (element) {
+    var forEach = function (array, callback, scope) {
+        var i;
+        for (i = 0; i < array.length; i += 1) {
+            callback.call(scope, array[i]);
+        }
+    };
+
+    var handleClick = function (element) {
         element.addEventListener('click', function (event) {
             event.preventDefault();
             var socialNetwork = this.dataset.socialNetwork;
-            trackLink(this, socialNetwork);
+            var shareUri = this.dataset.shareUri;
+            trackLink(shareUri, socialNetwork);
             window.open(this.getAttribute('href'), socialNetwork, 'height=500,width=575');
         }, false);
-    });
+    };
 
-    analyticsType = document.body.dataset.analyticsType;
+    // Share Windows
+    var socialLinks = document.querySelectorAll('.social');
+    if (socialLinks) {
+        forEach(socialLinks, handleClick);
+    }
 
 });
