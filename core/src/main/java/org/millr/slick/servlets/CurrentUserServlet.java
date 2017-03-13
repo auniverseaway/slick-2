44062c1453ca737db33ba2334ccc97c36265f8bf
Chris Millar <cmillar@adobe.com>
2017-03-13 10:53:50 -0600

    18 - Adding proper jackrabbit queries for user

--- core/src/main/java/org/millr/slick/servlets/CurrentUserServlet.java
+++ core/src/main/java/org/millr/slick/servlets/CurrentUserServlet.java
@@ -2,9 +2,16 @@ package org.millr.slick.servlets;
 
 import java.io.IOException;
 
+import javax.jcr.AccessDeniedException;
+import javax.jcr.RepositoryException;
 import javax.jcr.Session;
+import javax.jcr.UnsupportedRepositoryOperationException;
+import javax.jcr.Value;
 
 import org.apache.felix.scr.annotations.sling.SlingServlet;
+import org.apache.jackrabbit.api.JackrabbitSession;
+import org.apache.jackrabbit.api.security.user.User;
+import org.apache.jackrabbit.api.security.user.UserManager;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.resource.ResourceResolver;
@@ -23,6 +30,17 @@ public class CurrentUserServlet extends SlingAllMethodsServlet {
     protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) {
         ResourceResolver resolver = request.getResourceResolver();
         Session session = resolver.adaptTo(Session.class);
+        JackrabbitSession jsSession = (JackrabbitSession) session;
+        try {
+			final UserManager userManager = jsSession.getUserManager();
+			final User user = (User) userManager.getAuthorizable(session.getUserID());
+			String firstName = user.getProperty("firstName")[0].getString();
+		} catch (RepositoryException repositoryException) {
+			
+			repositoryException.printStackTrace();
+		}
+        
+        
         String userId = session.getUserID();
         try {
             response.getWriter().write(userId);
